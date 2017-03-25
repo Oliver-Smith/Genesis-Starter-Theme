ae9acabcdbcda5c992953f63acdacac58e3a2ce4
Oliver-Smith <olesuper8@gmail.com>
2017-03-25 14:14:58 +0000

    Update woocommerce-output.php

--- lib/components/woocommerce/woocommerce-output.php
+++ lib/components/woocommerce/woocommerce-output.php
@@ -12,53 +12,45 @@
 
 namespace RedThreadCreative\RedThreadChild\Customizer;
 
-use RedThreadCreative\RedThreadChild\Customizer as customizer;
-
-add_filter( 'woocommerce_enqueue_styles', 'woocommerce_styles' );
+use KnowTheCode\Developers\Customizer as customizer;
+add_filter( 'woocommerce_enqueue_styles', __NAMESPACE__ . '\enqueue_woocommerce_styles' );
 /**
  * Enqueue the theme's custom WooCommerce styles to the WooCommerce plugin.
  *
  * @since 2.3.0
  *
+ * @param array $enqueue_styles An array of styles
+ *
  * @return array Required values for the Genesis Sample Theme's WooCommerce stylesheet.
  */
-function woocommerce_styles( $enqueue_styles ) {
-
-	$enqueue_styles['woocommerce-styles'] = array(
-		'src'     => get_stylesheet_directory_uri() . '/lib/woocommerce/woocommerce.css',
+function enqueue_woocommerce_styles( $enqueue_styles ) {
+	$enqueue_styles[ get_woo_style_handle() ] = array(
+		'src'     => CHILD_URL . '/lib/components/woocommerce/assets/css/woocommerce.css',
 		'deps'    => '',
 		'version' => CHILD_THEME_VERSION,
 		'media'   => 'screen'
 	);
-
 	return $enqueue_styles;
-
 }
-
-add_action( 'wp_enqueue_scripts', 'woocommerce_css' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\build_incline_css_from_customizer_settings' );
 /**
- * Add the themes's custom CSS to the WooCommerce stylesheet.
+* Checks the settings for the link color, and accent color.
+* If any of these value are set the appropriate CSS is output.
+*
+* @since 1.0.0
  *
- * @since 2.3.0
- *
- * @return string CSS to be outputted after the theme's custom WooCommerce stylesheet.
- */
-function woocommerce_css() {
-
+ * @return void
+*/
+function build_incline_css_from_customizer_settings() {
 	// If WooCommerce isn't active, exit early.
 	if ( ! class_exists( 'WooCommerce' ) ) {
 		return;
 	}
-
 	$prefix = customizer\get_settings_prefix();
-	
 	$color_link = get_theme_mod( $prefix . '_link_color', customizer\get_default_link_color() );
-	$color_accent = get_theme_mod( $prefix . '_accent_color', customizer\get_default_accent_color() )
-
-	$woo_css = '';
-
-	$woo_css .= ( customizer\get_default_link_color() !== $color_link ) ? sprintf( '
-	
+	$color_accent = get_theme_mod( $prefix . '_accent_color', customizer\get_default_accent_color() );
+	$css = '';
+	$css .= ( customizer\get_default_link_color() !== $color_link ) ? sprintf( '
 		.woocommerce div.product p.price,
 		.woocommerce div.product span.price,
 		.woocommerce div.product .woocommerce-tabs ul.tabs li a:hover,
@@ -71,12 +63,8 @@ function woocommerce_css() {
 		.woocommerce .widget_layered_nav_filters ul li a::before,
 		.woocommerce .widget_rating_filter ul li.chosen a::before {
 			color: %s;
-		}
-
-	', $color_link ) : '';
-
-	$woo_css .= ( customizer\get_default_accent_color() !== $color_accent ) ? sprintf( '
-
+		', $color_link ) : '';
+	$css .= ( customizer\get_default_accent_color() !== $color_accent ) ? sprintf( '
 		.woocommerce a.button:hover,
 		.woocommerce a.button:focus,
 		.woocommerce a.button.alt:hover,
@@ -101,28 +89,22 @@ function woocommerce_css() {
 			background-color: %1$s;
 			color: %2$s;
 		}
-
 		.woocommerce-error,
 		.woocommerce-info,
 		.woocommerce-message {
 			border-top-color: %1$s;
 		}
-
 		.woocommerce-error::before,
 		.woocommerce-info::before,
 		.woocommerce-message::before {
 			color: %1$s;
 		}
-
-	', $color_accent, customizer\calculate_color_contrast( $color_accent ) ) : '';
-
-
-	if ( $woo_css ) {
-		wp_add_inline_style( 'woocommerce-styles', $woo_css );
+		', $color_accent, customizer\calculate_color_contrast( $color_accent ) ) : '';
+	if ( ! $css ) {
+		return;
 	}
-
+	wp_add_inline_style( get_woo_style_handle(), $css );
 }
-
 /**
  * Get styles handle for the WooCommerce module.
  *
